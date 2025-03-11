4fa4adce94775295b74508031a0c0dd18de2fbe8
musashi <1303maltsev@gmail.com>
2025-03-12 01:17:32 +0200

    debug#2 (fix button lock)

--- src/components/FormComponent/index.tsx
+++ src/components/FormComponent/index.tsx
@@ -3,11 +3,29 @@ import Selects from '../Selects'
 import Button from '../Button'
 import { FormPropsInterface, PokeResultsInterface } from '../../types/types'
 import InputField from '../InputField'
-import { usePokemons } from '../../hooks/hooks'
+import { onSubmitForm, usePokemons } from '../../hooks/hooks'
+import { useForm } from 'react-hook-form'
 
-const FormComponent = React.memo(({errors, isValid, handleSubmit, onSubmit, register, control}: FormPropsInterface) => {
-    const [countOffset, setCountOffset] = useState(0)
-    const { data, isLoading, isError } = usePokemons({offset: countOffset})
+const FormComponent = React.memo(({updateParams, searchParams, setIsOpen, isOpen}: FormPropsInterface) => {
+    const { data, fetchNextPage, isLoading, isError } = usePokemons()
+    const {
+        register,
+        control, 
+        handleSubmit, 
+        formState: { errors, isValid }
+    } = useForm({ 
+        defaultValues: {
+            firstName: searchParams.get('firstName') || "",
+            lastName: searchParams.get('lastName') || "",
+            Member1: searchParams.get('Member1') || "",
+            Member2: searchParams.get('Member2') || "",
+            Member3: searchParams.get('Member3') || "",
+            Member4: searchParams.get('Member4') || "",
+        }, 
+        mode: "onBlur",
+    });
+
+    const onSubmit = onSubmitForm({updateParams, setIsOpen, isOpen})
     return (
         <form className='flex flex-col shadow-md rounded-2xl p-10 bg-gray-100 border' onSubmit={handleSubmit(onSubmit)}>
             <h1 className='font-bold text-4xl mb-10'>Pokemon Team</h1>
@@ -23,7 +41,7 @@ const FormComponent = React.memo(({errors, isValid, handleSubmit, onSubmit, regi
                 error={errors.lastName} 
                 register={register('lastName', { required: true, minLength: 2, maxLength: 12, pattern: /^[A-Za-z]+$/i  })}
             />
-            <Selects data={data} control={control} errors={errors} setCountOffset={setCountOffset} offset={countOffset}/>
+            <Selects data={data} control={control} errors={errors} fetchNextPage={fetchNextPage}/>
             <div className='flex items-center justify-end mt-8'>
                 <Button type="submit" label="Submit" disabled={!isValid}/>
             </div>
